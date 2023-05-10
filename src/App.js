086bedae53c88c53c45cc0ebb94e22389847ddb6
edwin <ngisahedwin@gmail.com>
2023-05-10 19:21:57 +0300

    navbar styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Navbar from './Components/Navbar/Navbar';
+import './App.css'
+
+
 function App() {
   return (
     <div className="App">
-      Subscribe ZainKeepsCode
+      <Navbar />
     </div>
   );
 }
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='n-wrapper'>
+      <div className="n-left">
+        <div className="n-name">
+          Edwin
+
+        </div>
+        <span>toggle</span>
+      </div>
+      <div className="n-right">
+        <div className="n-list">
+          <ul>
+            <li>Home</li>
+            <li>Services</li>
+            <li>Experience</li>
+            <li>Portfolio</li>
+            <li>Testimonials</li>
+          </ul>
+        </div>
+        <button className="button">
+          contact me
+        </button>
+
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
